13610f5d1c9c3b4503daa3346b2e00e759219c9f
maumar <maumar@microsoft.com>
2022-07-13 22:30:53 -0700

    Gf

---
+++ src/EFCore.Relational/Metadata/Builders/JsonEntityTypeBuilder.cs
@@ -0,0 +1,69 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Builders
+{
+    /// <summary>
+    ///     Provides a simple API for configuring an entity mapped to a json column.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information and examples.
+    /// </remarks>
+    public class JsonEntityTypeBuilder
+    {
+        private readonly EntityTypeBuilder _entityTypeBuilder;
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        [EntityFrameworkInternal]
+        public JsonEntityTypeBuilder(EntityTypeBuilder entityTypeBuilder)
+        {
+            _entityTypeBuilder = entityTypeBuilder;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public virtual JsonEntityTypeBuilder HasJsonColumnTypeName(string typeName)
+        {
+            _entityTypeBuilder.Metadata.SetJsonColumnTypeName(typeName);
+
+            return this;
+        }
+
+        #region Hidden System.Object members
+
+        /// <summary>
+        ///     Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override string? ToString()
+            => base.ToString();
+
+        /// <summary>
+        ///     Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.</returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object? obj)
+            => base.Equals(obj);
+
+        /// <summary>
+        ///     Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode()
+            => base.GetHashCode();
+
+        #endregion
+    }
+}
